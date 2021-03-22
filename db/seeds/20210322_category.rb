377642f7c8354b6186ce8b472a25a1698b63d5cf
Ubuntu <ubuntu@ip-172-31-3-68.us-east-2.compute.internal>
2021-03-22 06:02:46 +0000

    aaa

---
+++ db/seeds/20210322_category.rb
@@ -0,0 +1,5 @@
+Category.create!([
+  {name: "チーズ", image_name: "cheese.jpg", category_id: "チーズ"},
+  {name: "肉", image_name: "meat.jpg", category_id: "肉類"},
+  {name: "魚介", image_name: "sea2.jpg", category_id: "魚介類"}
+])
---
+++ db/seeds/20210322_user.rb
@@ -0,0 +1,3 @@
+User.create!([
+  {name: "たけだきいち", email: "kt10195665@yahoo.ne.jp", password: "kiiti", image_name: "default_user.jpg"}
+])
