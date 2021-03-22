e6eef728e4ba6cb3bed0d2173bf3f595a7ad647d
Ubuntu <ubuntu@ip-172-31-3-68.us-east-2.compute.internal>
2021-03-22 06:44:26 +0000

    aaa

--- db/seeds/20210322_post.rb
+++ db/seeds/20210322_post.rb
@@ -1,5 +1,5 @@
 Post.create!([
-  {content: "ゴルゴンゾーラチーズ", #<ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition:0x000055c379e02558>: nil, numb: "3", user_id: 1, category_id: "チーズ"},
-  {content: "熟成ソーセージ", #<ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition:0x000055c379e02558>: nil, numb: "2", user_id: 1, category_id: "肉類"},
-  {content: "貝柱", #<ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition:0x000055c379e02558>: nil, numb: "4", user_id: 1, category_id: "魚介類"}
+  {content: "ゴルゴンゾーラチーズ", numb: "3", user_id: 1, category_id: "チーズ"},
+  {content: "熟成ソーセージ",  numb: "2", user_id: 1, category_id: "肉類"},
+  {content: "貝柱", numb: "4", user_id: 1, category_id: "魚介類"}
 ])
